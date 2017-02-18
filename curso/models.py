aedf1204c20d77c4e32691b5363e7a18671f3bd8
Ivan Mera <imera92@gmail.com>
2017-02-18 14:22:26 -0500

    Curso

---
+++ curso/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+	descripcion = models.TextField()
+
+	def __str__(self):
+		return self.seccion
+
+class Seccion(models.Model):
+	curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+	nombre = models.CharField(max_length=50)
+	descripcion = models.TextField()
+
+	def __str__(self):
+		return self.id + ' - ' + self.nombre
--- daw/urls.py
+++ daw/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('landing.urls')),
+    url(r'^curso/', include('curso.urls')),
 ]
