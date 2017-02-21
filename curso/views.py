8764b4dd916bc1e8713eb858f82df44673804518
Iván Mera <imera92@gmail.com>
2017-02-21 14:59:31 -0500

    crash

--- curso/views.py
+++ curso/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
-def curso(request)
-	return render(request, 'curso/curso.html')
+def curso(request):
+	curso = Curso.objects.get(id=1)
+	context = {
+		'descripcion' : curso.descripcion,
+	}
+	return render(request, 'curso/curso.html', context)
