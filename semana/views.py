03cc527b323000c16f8eee1e285d9924b126e0b6
Ivan Mera <imera92@gmail.com>
2017-02-22 05:58:13 -0500

    SEMANA - Configuración del control para el index

--- semana/views.py
+++ semana/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-	return render(request, 'semana/semana.html')
+	descripcion = Curso.objects.get(id=1).descripcion
+	planes = Plan.objects.all()
+	recursos = Recurso.objects.all()
+	context = {
+		'descripcion' : descripcion,
+		'planes' : planes,
+		'recursos' : recursos,
+	}
+	return render(request, 'semana/semana.html', context)
