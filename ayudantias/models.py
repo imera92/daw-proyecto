e29327966b27073ea0122ddc8e3f72cbbb372533
Ivan Mera <imera92@gmail.com>
2017-02-22 07:48:11 -0500

    AYUDANTIAS - Creando los modelos

--- ayudantias/models.py
+++ ayudantias/models.py
@@ -5,3 +5,20 @@ class Ayudantes(models.Model):
 	matricula = models.CharField(max_length=9, primary_key=True)
 	nombre = models.CharField(max_length=100)
 	correo = models.EmailField()
+
+class Aulas(models.Model):
+	codigo = CharField(primary_key=True)
+	latitud = FloatField(blank=False)
+	longitud = FloatField(blank=False)
+
+class Ayudantias(models.Model):
+	DAYS_CHOICES = (
+		("Lunes", "Lunes"),
+		("Martes", "Martes"),
+		("Miercoles", "Miercoles"),
+		("Jueves", "Jueves"),
+		("Viernes", "Viernes"),
+	)
+	ayudante = models.ForeignKey(Ayudantes, on_delete=models.CASCADE)
+	aula = models.ForeignKey(Aulas, on_delete=models.CASCADE)
+	dia = models.CharField(max_length=9, choise=DAYS_CHOICES, blank=False)
