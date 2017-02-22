263681543fc771b1e5a7d8322f01dde49e7a91b9
Ivan Mera <imera92@gmail.com>
2017-02-22 00:25:47 -0500

    Configurando modulo ADMIN

--- curso/admin.py
+++ curso/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Curso, Seccion
 
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Seccion)
--- semana/admin.py
+++ semana/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Curso, Plan, TipoRecurso, Recurso
 
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Plan)
+admin.site.register(TipoRecurso)
+admin.site.register(Recurso)
