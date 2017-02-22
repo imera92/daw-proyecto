bdf1325e60a7ed9a3394fd19bafa6970de0795c2
Ivan Mera <imera92@gmail.com>
2017-02-22 11:16:48 -0500

    AYUDANTIAS - Registro en modulo administrador

--- ayudantias/admin.py
+++ ayudantias/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Ayudantes, Aulas, Ayudantias
 
 # Register your models here.
+admin.site.register(Ayudantes)
+admin.site.register(Aulas)
+admin.site.register(Ayudantias)
