26470045937f31e706cbf120d878c7f5749e01fb
Ivan Mera <imera92@gmail.com>
2017-02-22 08:12:54 -0500

    EQUIPO - Modificando las vistas

--- equipo/views.py
+++ equipo/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import *
+from ayudantias.models import Ayudantes
 
 # Create your views here.
 def index(request):
-	return render(request, 'equipo/equipo.html')
+	coordinador = Coordinadores.objects.get(id=1)
+	profesores = Profesores.objects.all()
+	ayudantes = Ayudantes.objects.all()
+	context = {
+		'coordinador' : coordinador,
+		'profesores' : profesores,
+		'ayudantes' : ayudantes,
+	}
+	return render(request, 'equipo/equipo.html', context)
