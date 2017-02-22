bec963db082f1a9b2052b40aa173b309e6cebb98
Ivan Mera <imera92@gmail.com>
2017-02-22 07:12:08 -0500

    EQUIPO - Creando los modelos

--- equipo/models.py
+++ equipo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Coordinadores(models.Model):
+	nombre = models.CharField(max_length=100)
+	foto_archivo = models.FileField(blank=True, verbose_name="Foto (Archivo)")
+	foto_url = models.URLField(blank=True, verbose_name="Foto (URL)")
+	correo = models.EmailField()
+
+class Profesores(models.Model):
+	nombre = models.CharField(max_length=100)
+	foto_archivo = models.FileField(blank=True, verbose_name="Foto (Archivo)")
+	foto_url = models.URLField(blank=True, verbose_name="Foto (URL)")
+	correo = models.EmailField()
