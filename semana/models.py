80964920ca55197027b7e56bc3462efd78572a5f
Ivan Mera <imera92@gmail.com>
2017-02-22 05:55:46 -0500

    SEMANA - Modificación en los modelos

--- semana/models.py
+++ semana/models.py
@@ -5,26 +5,28 @@ class Curso(models.Model):
 	descripcion = models.TextField()
 
 	def __str__(self):
-		return self.descripcion
+		return str(self.id)
 
 class Plan(models.Model):
 	curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
 	nombre = models.CharField(max_length=50)
 
 	def __str__(self):
-		return str(self.id) + ' - ' + self.nombre
+		return self.nombre
 
 class TipoRecurso(models.Model):
 	nombre = models.CharField(max_length=20)
+	icono = models.FileField()
 
 	def __str__(self):
-		return str(self.id) + ' - ' + self.nombre
+		return self.nombre
 		
 class Recurso(models.Model):
 	plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
 	tipo = models.ForeignKey(TipoRecurso, on_delete=models.CASCADE)
 	nombre = models.CharField(max_length=50)
-	url = models.FileField()
+	archivo = models.FileField(blank=True)
+	url = models.URLField(blank=True)
 
 	def __str__(self):
 		return str(self.id) + ' - ' + self.nombre +  ' - ' + str(self.tipo)
