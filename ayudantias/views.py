ec039aaa09986918c28b2498f0d2130597a8ade3
Ivan Mera <imera92@gmail.com>
2017-02-22 08:57:49 -0500

    AYUDANTIAS - Configurando el controlador para el index

--- ayudantias/views.py
+++ ayudantias/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-	return render(request, 'ayudantias/ayudantias.html')
+	ayudantes = Ayudantes.object.all()
+	ayudantias = Ayudantias.object.all()
+	context = {
+		'ayudantes' : ayudantes,
+		'ayudantias' : ayudantias,
+	}
+	return render(request, 'ayudantias/ayudantias.html', context)
