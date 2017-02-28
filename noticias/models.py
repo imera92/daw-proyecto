017c5835a65f78da9993ecf2fee06ca1a90ac75f
Ivan Mera <imera92@gmail.com>
2017-02-28 18:29:32 -0500

    NOTICIAS - Creando los modelos

--- noticias/models.py
+++ noticias/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 # Create your models here.
+class Noticia(models.Model):
+	titulo = models.CharField(max_length=50)
+	fecha = models.DateField(auto_now_add=True)
+	descripcion = tinymce_models.HTMLField()
+
+	def __str__(self):
+		return self.titulo + ' - ' + self.fecha
