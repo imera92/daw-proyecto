bfea7b74695e0cfb4fb08aaf374e79e285914a6c
Ivan Mera <imera92@gmail.com>
2017-02-28 23:49:31 -0500

    NOTICIAS - Cambios en modelo NOTICIA

--- noticias/models.py
+++ noticias/models.py
@@ -3,9 +3,9 @@ from tinymce import models as tinymce_models
 
 # Create your models here.
 class Noticia(models.Model):
-	titulo = models.CharField(max_length=50)
+	titulo = models.CharField(max_length=100)
 	fecha = models.DateField(auto_now_add=True)
 	descripcion = tinymce_models.HTMLField()
 
 	def __str__(self):
-		return self.titulo + ' - ' + self.fecha
+		return self.titulo + ' - ' + str(self.fecha)
