51fc5380247e902c970ab45ed3defaefbe9ba2c6
Ivan Mera <imera92@gmail.com>
2017-03-01 23:04:26 -0500

    CURSO - Cambios en el modelo

--- curso/models.py
+++ curso/models.py
@@ -9,10 +9,15 @@ class Curso(models.Model):
 		return self.descripcion
 
 class Seccion(models.Model):
+	STATE_CHOICES = (
+		("active", "Activo"),
+		("inactive", "Inactivo"),
+	)
 	curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
 	nombre = models.CharField(max_length=50)
 	descripcion = tinymce_models.HTMLField()
-	estado = models.CharField(max_length=8)
+	estado = models.CharField(max_length=8, choices=STATE_CHOICES, blank=False)
+	indice = models.PositiveSmallIntegerField()
 
 	def __str__(self):
-		return str(self.id) + ' - ' + self.nombre
+		return str(self.indice) + ' - ' + self.nombre + ' - ' + self.estado
