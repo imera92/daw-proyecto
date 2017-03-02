f380327a1677e781cc77fddc9218d12877f862cb
Ivan Mera <imera92@gmail.com>
2017-03-01 23:05:18 -0500

    CURSO - Cambiando el controlador para ordenar secciones de forma ascendente

--- curso/views.py
+++ curso/views.py
@@ -4,7 +4,7 @@ from .models import Curso, Seccion
 # Create your views here.
 def curso(request):
 	curso = Curso.objects.get(id=1)
-	secciones = Seccion.objects.all()
+	secciones = Seccion.objects.order_by('indice')
 	context = {
 		'descripcion' : curso.descripcion,
 		'secciones' : secciones,
