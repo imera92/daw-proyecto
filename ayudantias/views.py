a00294b82553c6d01d372d2a3e226fa54e9efc53
Ivan Mera <imera92@gmail.com>
2017-03-02 10:43:11 -0500

    AYUDANTIAS - Cambio en controlador INDEX para incluir todas las instancias de APIEMBED

--- ayudantias/views.py
+++ ayudantias/views.py
@@ -4,9 +4,11 @@ from .models import *
 # Create your views here.
 def index(request):
 	ayudantias = Ayudantias.objects.all()
+	embed = APIembed.objects.all()
 	dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes']
 	context = {
 		'ayudantias' : ayudantias,
 		'dias' : dias,
+		'embed' : embed,
 	}
 	return render(request, 'ayudantias/ayudantias.html', context)
