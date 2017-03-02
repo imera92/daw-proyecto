44c177c8b9f6070d3153bb5448a4ef24289a3ca5
Ivan Mera <imera92@gmail.com>
2017-03-02 10:42:29 -0500

    AYUDANTIAS - Registro del modelo APIEMBED en modulo adminsitrador

--- ayudantias/admin.py
+++ ayudantias/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Ayudantes, Aulas, Ayudantias
+from .models import Ayudantes, Aulas, Ayudantias, APIembed
 
 # Register your models here.
 admin.site.register(Ayudantes)
 admin.site.register(Aulas)
 admin.site.register(Ayudantias)
+admin.site.register(APIembed)
